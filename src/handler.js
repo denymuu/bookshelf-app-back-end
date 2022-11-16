5a982108e0f9c3e1d34e13b1b34acdb98b8886f5
Deny Muhammad <denymuh9@gmail.com>
2022-11-16 10:39:55 +0700

    add all files and resource

---
+++ src/handler.js
@@ -0,0 +1,37 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {
+    const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+    const id = nanoid(16);
+    const finished = pageCount === readPage;
+    const insertedAt = new Date().toISOString();
+    const updatedAt = insertedAt;
+
+    const newBook ={ name, year, author, summary, publisher, pageCount, readPage, reading, id, finished, insertedAt, updatedAt,
+        
+    };
+
+    books.push(newBook);
+    const isSuccess = books.filter((book) => book.id === id). length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status : 'success',
+            message : 'Buku berhasil ditambahkan',
+            data : {
+                bookId : id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+    const response = h.response({
+        status : 'fail',
+        message : 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+
+};
+module.exports = { addBookHandler };
